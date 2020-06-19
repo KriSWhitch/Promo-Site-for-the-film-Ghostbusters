44f75f49bcda5a9ef65eaa65b40765fcfa260d24
KriSWhitch <krisfragerfeed@gmail.com>
2020-06-19 10:19:06 +0300

    Adding Slider and Actor/Creator swither

--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
 
-// Mobile Menu
+
 $(document).ready(function () {
+    // Mobile Menu
     const mMenuBtn = $(".m-menu-button");
     const mMenu = $(".m-menu");
     mMenuBtn.on("click", function(){
         mMenu.toggleClass("active");
         $("body").toggleClass("no-scroll");
-    })
+    });
+
+    // Burger Animation
+    $(".m-menu-button").click(function () {
+        $(this).toggleClass("active");
+    });
+
+    const tab = $(".tab");
+
+    tab.on("click", function(){
+        tab.removeClass("active");
+        $(this).toggleClass("active");
+        let activeTabContent = $(this).attr("data-target");
+        $(".tabs-content").removeClass("visible");
+        $(activeTabContent).toggleClass("visible");
+    });
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        slidesPerView: 4,
+        loop: true,
+        spaceBetween: 25,
+        breakpoints:{
+            992: {
+                slidesPerView: 4,
+            },
+            780: {
+                slidesPerView: 2,
+            },
+            320: {
+                slidesPerView: 1,
+                navigation: {
+                    nextEl: ".button-next",
+                },
+            },
+        }
+      })
+
 });
 
 
-// Burger Animation
-$(".m-menu-button").click(function () {
-    $(this).toggleClass("active");
-});
